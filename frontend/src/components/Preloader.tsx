e6437d95b2dbe9370533aa837314804bd44d33a7
ayushxnir <ayushxnir@gmail.com>
2025-12-31 17:29:59 +0545

    working on verdict frontend

---
+++ frontend/src/components/Preloader.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Preloader = () => {
+  return (
+    <>
+      {/* <div className="preloader" id="preloader">
+        <div className="loader">
+          <div className="line-scale-pulse-out-rapid">
+            <div> </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div> </div>
+          </div>
+        </div>
+      </div> */}
+    </>
+  )
+}
+
+export default Preloader
